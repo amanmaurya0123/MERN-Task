f3536aa0f9796d1accb753ae4b1eeefd2e3d02d1
amanmaurya0123 <118646086+amanmaurya0123@users.noreply.github.com>
2024-11-19 09:20:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,14 +7,14 @@ const transactionRoutes = require("./routes/transactionRoutes");
 const app = express();
 const PORT = 5000;
 
-// Middleware
+
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
+
 app.use("/api", transactionRoutes);
 
-// Connect to MongoDB
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/transactions", {
     useNewUrlParser: true,
@@ -23,7 +23,7 @@ mongoose
   .then(() => console.log("Connected to MongoDB"))
   .catch((error) => console.error("Error connecting to MongoDB:", error));
 
-// Start server
+
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`)
 );
